f56989e5e3b7c2e534413d2facb1fc84203a00e3
Ricardo <rmvnew@gmail.com>
2025-06-25 20:00:59 -0400

    fix

--- src/screens/ProductFormScreen.tsx
+++ src/screens/ProductFormScreen.tsx
@@ -364,7 +364,7 @@ export default function ProductFormScreen() {
                     }
                 </TouchableOpacity>
             </ScrollView>
-            <Toast />
+
         </>
     );
 }
